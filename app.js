f5ee3519136df28005ac305c802cc54f8ec00245
Ahmed Zubair <153666614+AhmedZubair18@users.noreply.github.com>
2024-09-01 01:01:31 +0500

    Quiz app

--- app.js
+++ app.js
@@ -1,21 +1,41 @@
+// const boldBtn = document.getElementById("boldBtn")
+// const itelicBtn = document.getElementById("itelicBtn")
+// const underlineBtn = document.getElementById("underlineBtn")
+// const editor = document.getElementById("editor")
+
+
+// function execCommand (command ,value = null){
+//     document.execCommand(command ,false,value)
+// }
+
+// boldBtn.addEventListener('click' , () => execCommand('bold') )
+// itelicBtn.addEventListener('click' , () => execCommand ('italic'))
+// underlineBtn.addEventListener('click', () => execCommand('underline'))
+
+// function abc(){
+//     setTimeout(function(){
+//         alert("Please note: The editor will only operate on the selected text.")
+//     },1000)
+
+// }
+// abc()
+
+
 const boldBtn = document.getElementById("boldBtn")
-const itelicBtn = document.getElementById("itelicBtn")
+const italicBtn = document.getElementById("italicBtn")
 const underlineBtn = document.getElementById("underlineBtn")
 const editor = document.getElementById("editor")
 
-
-function execCommand (command ,value = null){
-    document.execCommand(command ,false,value)
+function execCommand (command,value=null){
+    document.execCommand(command,false,value)
 }
+boldBtn.addEventListener("click" , () => execCommand ("bold"))
+italicBtn.addEventListener("click",() => execCommand("italic"))
+underlineBtn.addEventListener("click",() => execCommand("underline"))
 
-boldBtn.addEventListener('click' , () => execCommand('bold') )
-itelicBtn.addEventListener('click' , () => execCommand ('italic'))
-underlineBtn.addEventListener('click', () => execCommand('underline'))
-
-function abc(){
+function abc (){
     setTimeout(function(){
         alert("Please note: The editor will only operate on the selected text.")
     },1000)
-
 }
 abc()
